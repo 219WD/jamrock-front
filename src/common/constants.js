fd93f7770bfb08fd099bb113899c9ca425058c1d
Juan Canepa <macgunmanmc@gmail.com>
2025-09-19 14:28:42 -0300

    correccion

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,4 @@
-const getApiUrl = () => {
-  if (window.location.hostname === "localhost") {
-    return "http://localhost:4000";
-  } else {
-    return "https://jamrock-back.onrender.com";
-  }
-};
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:4000";
 
-const API_URL = getApiUrl();
 export default API_URL;
